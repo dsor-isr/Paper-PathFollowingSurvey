259ee351934be33a7720becee58f374fab056e28
Marcelo Jacinto <marcelo.jacinto@tecnico.ulisboa.pt>
2022-01-22 21:03:09 +0000

    feat: passing utm coordinates for the path in launch file

--- experiments_bringup/src/mission_launcher.py
+++ experiments_bringup/src/mission_launcher.py
@@ -11,6 +11,10 @@ if __name__ == "__main__":
     vehicle_name = rospy.get_param('vehicle_name', 'myellow')
     path_type = rospy.get_param('path_type', 'bernoulli')
     controller_type = rospy.get_param('controller_type', 'aguiar')
+
+    # Get the UTM coordinates where the inertial frame is located
+    utm_north = float(rospy.get_param('utm_north', '4290794.432828876'))
+    utm_east = float(rospy.get_param('utm_east', '491936.56077393395'))
     
     # Wait for the required path services to start
     for service in pth.path_services_required:
@@ -27,7 +31,7 @@ if __name__ == "__main__":
     if path_type == 'lawn_mower':
         pth.spawn_lawn_mower(0.0, vehicle_name)
     elif path_type == 'bernoulli':
-        pth.spawn_bernoulli(30, 4290841.0, 491926.0,-30, vehicle_name)
+        pth.spawn_bernoulli(radius=30, center_x=utm_north-30.0, center_y=utm_north-30.0, z=0.0, vehicle_name=vehicle_name)
     else:
         rospy.WARN('Desired path only supports lawn_mower or bernoulli in this demo!')
         rospy.signal_shutdown('Desired path not supported')
